54a968cc868a351acc9d8a2d858a45367c24752e
clardizabal <chris.lardizabal@gmail.com>
2016-09-02 20:40:13 -0700

    change colors of squares on click

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
-var colors = ['black', 'blue', 'cyan', 'green', 'magenta', 'red', 'yellow'];
+const colors = ['black', 'blue', 'cyan', 'green', 'magenta', 'red', 'yellow'];
 
 class Square extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
+      /* Generate a randome color from list of colors */
       color: colors[Math.floor(Math.random() * colors.length)],
+      textColor: 'white',
     };
+
+    this.changeColor = this.changeColor.bind(this);
   }
 
 
+  changeColor() {
+    let index = colors.indexOf(this.state.color);
+    index = index < colors.length - 1 ? index + 1 : 0;
+    this.setState({color: colors[index]});
+  }
+
   render() {
-    var divStyle = {
-      background: this.state.color
+    /* Inline styling */
+    const squareStyle = {
+      background: this.state.color,
+    }
+    const textStyle = {
+      color: this.state.color === 'yellow' ? 'black' : 'white',
     }
 
     return (
-      <div className="square" style={divStyle}>
-        <div className="colorText">{this.state.color}</div>
+      <div className="square" style={squareStyle} onClick={this.changeColor}>
+        <div className="colorText" style={textStyle}>{this.state.color}</div>
       </div>
     );
   }
 }
 
-var TableRow = () => (
+const TableRow = () => (
   <tr>
     <td>
       <Square/>
@@ -39,20 +54,20 @@ var TableRow = () => (
   </tr>
 );
 
-var App = () => (
+const App = () => (
     <table>
-    <tbody>
-      <TableRow/>
-      <TableRow/>
-      <TableRow/>
-      <TableRow/>
-      <TableRow/>
-      <TableRow/>
-      <TableRow/>
-      <TableRow/>
-      <TableRow/>
-      <TableRow/>
-    </tbody>
+      <tbody>
+        <TableRow/>
+        <TableRow/>
+        <TableRow/>
+        <TableRow/>
+        <TableRow/>
+        <TableRow/>
+        <TableRow/>
+        <TableRow/>
+        <TableRow/>
+        <TableRow/>
+      </tbody>
    </table>
 );
 
